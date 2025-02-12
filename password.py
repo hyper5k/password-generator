b7b19189410c2d42efcdad93d61a7c9e15f81972
kurt pablico <kurtpablic0@gmail.com>
2025-02-12 18:39:31 +0000

    this is a password generator

--- password.py
+++ password.py
@@ -7,7 +7,8 @@ def random_character():
     choices = string.ascii_letters + string.digits + string.punctuation
     return random.choice(choices)
 
-passwordLength = 25
+passwordLength =  input("how long should your password be")
+passwordLength = int (passwordLength)
 
 def generate_strong_password():
     password = ""
@@ -25,4 +26,27 @@ def fetch_word():
     word = response.json()[0]
     return word
 
-print(fetch_word())
+def replaceLetters(word):
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+    if "f" in word:
+        word = word.replace("f", "%") 
+    if "e" in word:
+        word = word.replace("e", "P")
+    if "o" in word:
+        word = word.replace("o", "$")
+
+    return word
+
+
+
+def generate_weaker_passoword():
+    word1 = fetch_word()
+    word2 = fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
+    passoword = word1 + word2
+    return passoword
+
+print(generate_weaker_passoword())
